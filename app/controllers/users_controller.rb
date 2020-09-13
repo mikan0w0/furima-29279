d717a83f35a9ba663c009d3443c0340e248a3f87
karasuyamamika <j.girl.mi@gmail.com>
2020-09-13 16:34:52 +0900

    itemsテーブル作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
-  
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email)
+  end
+
 end
