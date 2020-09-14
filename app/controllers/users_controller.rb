6530b07ae4c2c864e2e7fc285818a76f04b140ec
karasuyamamika <j.girl.mi@gmail.com>
2020-09-14 16:01:48 +0900

    モデル単体テストコード２

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
-
-
+  def create
+    @user = User.new(user_params)
+    #バリデーションで問題があれば、保存はされず「投稿画面」に戻る
+    if @user.valid?
+      @user.save
+      redirect_to new_user_registration_path
+    else
+      #保存されなければ、newに戻る
+      render 'new(@user)'
+    end
+  end
   def update
     if current_user.update(user_params)
       redirect_to root_path
