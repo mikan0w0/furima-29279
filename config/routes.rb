6ea7799cff1cd8168ab451d06b14ac63f3c6597f
karasuyamamika <j.girl.mi@gmail.com>
2020-09-14 19:17:52 +0900

    itemsコントローラーとルーティングの変更2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-=======
   devise_for :users
   root to: "items#index"
   resources :users
   resources :items
->>>>>>> Stashed changes
 end
