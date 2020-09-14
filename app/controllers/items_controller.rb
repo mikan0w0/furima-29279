f26365d48f7e60e10a2f9a9439eed5d3080ff22f
karasuyamamika <j.girl.mi@gmail.com>
2020-09-14 21:57:22 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,25 +1,15 @@
 class ItemsController < ApplicationController
 
   before_action :move_to_index, except: [:index, :show, :search]
-<<<<<<< Updated upstream
   def new
     @item = Item.new
   end
   def create
     @item = Item.new(item_params)
-    #バリデーションで問題があれば、保存はされず「投稿画面」に戻る
     if @item.valid?
       @item.save
-=======
-  def create
-    @tweet = Tweet.new(tweet_params)
-    #バリデーションで問題があれば、保存はされず「投稿画面」に戻る
-    if @tweet.valid?
-      @tweet.save
->>>>>>> Stashed changes
       redirect_to root_path
     else
-      #保存されなければ、newに戻る
       render 'new'
     end
   end
