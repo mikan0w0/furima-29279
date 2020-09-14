99bb921687ef61431b559dd7b303f1dba986a16d
karasuyamamika <j.girl.mi@gmail.com>
2020-09-14 19:37:30 +0900

    出品画面の遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,20 @@
 class ItemsController < ApplicationController
 
   before_action :move_to_index, except: [:index, :show, :search]
-  # def create
-  #   @tweet = Tweet.new(tweet_params)
-  #   #バリデーションで問題があれば、保存はされず「投稿画面」に戻る
-  #   if @tweet.valid?
-  #     @tweet.save
-  #     redirect_to root_path
-  #   else
-  #     #保存されなければ、newに戻る
-  #     render 'new'
-  #   end
-  # end
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    #バリデーションで問題があれば、保存はされず「投稿画面」に戻る
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      #保存されなければ、newに戻る
+      render 'new'
+    end
+  end
   def index
     @items = Item.all()
   end
@@ -21,6 +24,7 @@ class ItemsController < ApplicationController
     unless user_signed_in?
       redirect_to action: :index
     end
+  end
   def index
   end
 end
