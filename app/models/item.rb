8510e518aad56de6f5b7b442fb1684a901945055
karasuyamamika <j.girl.mi@gmail.com>
2020-09-15 10:58:29 +0900

    1商品に1画像が必須の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
-  validates :text, presence: true
 
   has_one :user
   has_one :item_transaction
   has_one_attached :image
+
+  validates :text, presence: true
+  validates :self.image.attached?, presence: true
 end
