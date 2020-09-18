68460a01f540ea4469445d7554560231c89c4026
karasuyamamika <j.girl.mi@gmail.com>
2020-09-18 22:44:10 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show, :search]
   def new
-    @items = Item.new
+    @item = Item.new
   end
   def create
     @item = Item.new(item_params)
