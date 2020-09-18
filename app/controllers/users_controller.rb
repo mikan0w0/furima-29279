66b7780f0f68d34d096a8e0564234803f957ec84
karasuyamamika <j.girl.mi@gmail.com>
2020-09-18 19:24:33 +0900

    デプロイへの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,7 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  def update
-    if current_user.update(user_params)
-      redirect_to root_path
-    else
-      render :index
-    end
-  end
-
+  
   private
 
   def user_params
