c58acb825410b52e31db4b72b87b5b7b79c248e9
karasuyamamika <j.girl.mi@gmail.com>
2020-09-20 17:46:20 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,20 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+  def edit
+    @item = Item.find(params[:id])
+  end
+  def update
+    @item =Item.find(params[:id])
+    @item.update(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
 
   private
 
