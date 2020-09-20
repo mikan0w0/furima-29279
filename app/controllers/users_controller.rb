0ecaac3fec932ad4ff26f44361d7558ccb17d246
karasuyamamika <j.girl.mi@gmail.com>
2020-09-20 14:46:31 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,6 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  def update
-    if current_user.update(user_params)
-      redirect_to root_path
-    else
-      render :index
-    end
-  end
 
   private
 
