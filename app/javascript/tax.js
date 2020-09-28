48ee4202ae83d87cc3bcb477dc5e7d9d7d395e48
karasuyamamika <j.girl.mi@gmail.com>
2020-09-28 16:50:45 +0900

    JSファイル読み込みの変更

--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -1,14 +1,17 @@
-function tax(){
-  const priceinput = document.getElementById("item-price");
-  const taxprice = document.getElementById("add-tax-price");
-  const putprofit = document.getElementById("profit");
-  
-  priceinput.addEventListener("input", function(){
-    const price = priceinput.value
-    const tax = Math.round(price * 0.10);
-    const profit = (price - tax);
-    taxprice.innerHTML = tax;
-    putprofit.innerHTML = profit;
-  })
-}
-window.addEventListener("load", tax);
+
+if (document.URL.match( /new/) || document.URL.match( /edit/)) {
+  function tax(){
+    const priceinput = document.getElementById("item-price");
+    const taxprice = document.getElementById("add-tax-price");
+    const putprofit = document.getElementById("profit");
+    
+    priceinput.addEventListener("input", function(){
+      const price = priceinput.value
+      const tax = Math.round(price * 0.10);
+      const profit = (price - tax);
+      taxprice.innerHTML = tax;
+      putprofit.innerHTML = profit;
+    });
+  };
+  window.addEventListener("load", tax);
+};
